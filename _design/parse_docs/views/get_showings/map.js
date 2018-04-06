f754d59b22206f9604474f598021445641a691da
Gabriel Nagy <gabrielnagy@me.com>
2018-04-06 12:21:00 +0300

    Fix unexpected date format change

--- _design/parse_docs/views/get_showings/map.js
+++ _design/parse_docs/views/get_showings/map.js
@@ -3,7 +3,7 @@ function (doc) {
         doc.body.films.forEach(function(film) {
             doc.body.events.forEach(function(event) {
                 if (film.id == event.filmId) {
-                    emit([doc._id, event.eventDateTime.split(" ")[1], film.name, event.bookingLink, event.attributeIds, film.length, film.releaseYear], event.eventDateTime.split(" ")[1]);
+                    emit([doc._id, event.eventDateTime.split(" ")[1] || event.eventDateTime.split("T")[1].split(":").slice(0, -1).join(":"), film.name, event.bookingLink, event.attributeIds, film.length, film.releaseYear], event.eventDateTime.split(" ")[1] || event.eventDateTime.split("T")[1].split(":").slice(0, -1).join(":"));
                 }
             });
         });
